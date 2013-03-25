2867bf2bd9bddbad1d1357441d492e6982d738d0
Thanasis Polychronakis <thanpolas@gmail.com>
2013-03-25 16:54:10 +0200

    adding process tests

--- test/spec/processJobs.test.js
+++ test/spec/processJobs.test.js
@@ -40,7 +40,47 @@ suite('Job Processing', function() {
       cb();
       done();
     });
-
   });
+
+  test('Concurent jobs', function(done) {
+    // allow some time to execute
+    this.timeout(10000);
+
+    // create 20 jobs
+    var jobPromises = [];
+    for (var i = 0; i < 20; i++) {
+      jobPromises.push(kickq.create('process-test-concurent'));
+    }
+
+    var jobProcessCount = 0;
+    var jobProcessQueue = [];
+    function startProcess() {
+      kickq.process('process-test-concurent', 10, function(jobObj, data, cb) {
+        jobProcessCount++;
+        jobProcessQueue.push(cb);
+      });
+    }
+
+    when.all(jobPromises).then(startProcess);
+
+    // allow 2s for all to-process jobs to be collected
+    setTimeout(function(){
+      assert.equal(jobProcessCount, 10, '10 jobs should be queued for processing');
+      done();
+    }, 2000);
+  });
+
+  test('Process creates ghost by never reporting outcome', function(done){
+    kickq.create('process-ghost');
+    kickq.process('process-ghost', function(jobObj, data, cb) {});
+
+    // give time for jobs to process
+    setTimeout(function(){
+      ok(false, 'TODO create a test case for ghost jobs');
+      done();
+    }, 300);
+  });
+
+
 });
 
