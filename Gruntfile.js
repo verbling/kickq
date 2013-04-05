ccfa3adfeb4972a5823694fb9337b3bac3a5c9a6
Thanasis Polychronakis <thanpolas@gmail.com>
2013-04-05 16:26:31 +0300

    prepping grunt for tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,18 +7,12 @@
  * Licensed under the MIT license.
  */
 
-var config = require('config'),
-    gruntAssets = require('./tasks/task-main');
-
 module.exports = function( grunt ) {
   'use strict';
 
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-s3');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-contrib-clean');
-
-  gruntAssets(grunt);
 
   //
   // Grunt configuration:
@@ -26,151 +20,10 @@ module.exports = function( grunt ) {
   //
   grunt.initConfig({
 
-    assets: {
-      options: {
-        debug: true,
-        truncateHash: 8,
-        manifest: 'temp/manifest.json',
-        cdnurl: 'http://s3.aws.com/testdelete/',
-        maxOperations: 0,
-        progress: false,
-        rel: 'lib/'
-      },
-
-      testCase: {
-        options: {
-          rel: 'test/case/',
-          truncateHash: 6,
-          manifest: 'temp/testManifest.json',
-          // force serialization of files so their order won't change on the
-          // manifest file.
-          maxOperations: 1
-        },
-        src: [
-          'test/case/**',
-          '!test/case/less/**',
-          '!test/case/handlebars/**'
-        ],
-        dest: 'temp/testCase'
-      },
-
-      testCaseAbs: {
-        options: {
-          rel: 'test/case/',
-          truncateHash: 6,
-          manifest: 'temp/testManifestAbs.json',
-          // force serialization of files so their order won't change on the
-          // manifest file.
-          maxOperations: 1,
-          prepend: '/'
-        },
-        src: [
-          'test/case/**',
-          '!test/case/less/**',
-          '!test/case/handlebars/**'
-        ],
-        dest: 'temp/testCaseAbs'
-      }
-    },
-
-    assetsReplace: {
-      options: {
-        manifest: 'temp/testManifest.json'
-      },
-      testCase: {
-        options: {
-          key: '__ASSET(%)'
-        },
-        files: {
-          'temp/replace-testCase/': ['test/case/less/*.less']
-        }
-      },
-      testCaseHbs: {
-        options: {
-          key: '{{asset "%"}}'
-        },
-        files: {
-          'temp/handlebars/': ['test/case/handlebars/*.hbs']
-        }
-      },
-      testCasePrepend: {
-        options: {
-          key: '__ASSET(%)',
-          manifest: 'temp/testManifestAbs.json',
-          prepend: '/'
-        },
-        files: {
-          'temp/replace-prepend/': ['test/case/less/*.less']
-        }
-      }
-    },
-
-    assetsBundle: {
-      options: {
-        manifest: 'temp/testManifest.json',
-        debug: false,
-        assets:[
-          'img/coding-weapons.png',
-          'img/social-white-v2.png',
-          'img/pdf-icon-bare.png'
-        ]
-      },
-      testCase: {
-        options: {
-        },
-        dest: 'temp/bundles/noexport.js'
-      },
-      testCaseNS: {
-        options: {
-          ns: 'car.zit.pof',
-        },
-        dest: 'temp/bundles/namespace.js'
-      },
-      testCaseAMD: {
-        options: {
-          amd: true,
-        },
-        dest: 'temp/bundles/amd.js'
-      }
-
-
-    },
-    assetsS3: {
-      options: {
-        debug: false,
-        checkS3Head: true,
-        manifest: 'temp/testManifest.json',
-        key: config.aws_key,
-        secret: config.aws_secret,
-        bucket: config.aws_static_bucket,
-        access: 'public-read',
-        progress: false
-      },
-      // Files to be uploaded.
-      target: {
-        // These options override the defaults
-        options: {
-          maxOperations: 0
-        },
-        upload: {
-          src: 'temp/testCase/**',
-          dest:  'testdelete/',
-          rel: 'temp/testCase/test/case',
-          gzip: true,
-          gzipExclude: ['.jpeg', '.jpg', '.png', '.gif', '.less', '.mp3',
-              '.mp4', '.mkv', '.webm', '.gz'],
-          headers: {'Cache-Control': 'max-age=31536000, public'}
-        }
-      }
-    },
-
     watch: {
       debug: {
         files: ['*.js', 'lib/**/*.js', 'tasks/**/*.js'],
         tasks: [
-        'assetsBundle:testCase'
-        //'assetsReplace:testCaseHbs'
-        //'assetsS3'
         ]
       },
       test: {
@@ -179,39 +32,18 @@ module.exports = function( grunt ) {
       }
     },
 
-    s3: {
-      options: {
-        key: config.aws_key,
-        secret: config.aws_secret,
-        bucket: config.aws_static_bucket,
-        access: 'public-read',
-        maxOperations: 100
-      },
-
-      // Files to be uploaded.
-      dev: {
-        upload: [
-          {
-            src: 'temp/assets/**',
-            dest:  'v/',
-            rel: 'temp/assets',
-            headers: {'Cache-Control': 'max-age=31536000, public'}
-          }
-        ]
-      }
-    },
-
     /**
      * TESTING
      *
      */
     clean: ['temp/*'],
+
     mochaTest: {
-      gruntTasks: [ 'test/spec/**/*.js' ]
+      itterative: [ 'test/spec/mainAPI.js' ]
     },
 
     mochaTestConfig: {
-      gruntTasks: {
+      itterative: {
         options: {
           reporter: 'nyan'
         }
@@ -223,13 +55,6 @@ module.exports = function( grunt ) {
 
   grunt.registerTask('test', [
     'clean',
-    'assets:testCase',
-    'assets:testCaseAbs',
-    'assetsBundle:testCase',
-    'assetsBundle:testCaseNS',
-    'assetsBundle:testCaseAMD',
-    'assetsReplace:testCase',
-    'assetsReplace:testCasePrepend',
     'mochaTest'
   ]);
 
