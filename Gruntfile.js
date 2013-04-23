7884184084a986d2853328a187c6951061c11159
Thanasis Polychronakis <thanpolas@gmail.com>
2013-04-23 21:20:11 +0300

    loadnpm task...

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,7 @@ module.exports = function( grunt ) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-release');
   grunt.loadTasks('tasks');
 
   //
