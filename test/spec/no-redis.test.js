7971defe5323c28e9b925e308eb5c5f603640f54
Thanasis Polychronakis <thanpolas@gmail.com>
2015-08-12 21:19:04 +0300

    Introduce no-redis test

---
+++ test/spec/no-redis.test.js
@@ -0,0 +1,37 @@
+/**
+ * @fileOverview Test kickq when there's no connection
+ */
+
+var sinon  = require('sinon');
+var expect = require('chai').expect;
+var grunt  = require('grunt');
+var assert = require('chai').assert;
+
+var kickq  = require('../../');
+var tester = require('../lib/tester');
+
+
+suite.only('No Redis', function() {
+  setup(function() {
+    kickq.reset();
+    kickq.config({
+      redisPort: 55555,
+    });
+    kickq.config('loggerConsole', true);
+    kickq.config('loggerLevel', 100);
+  });
+
+  teardown(function() {
+    kickq.reset();
+  });
+
+  test('Should not go berserk', function(done) {
+    var jobid;
+    console.log('GO');
+    kickq.process('process-test-one', function(job, data, cb) {
+      console.log('WTF');
+      cb();
+      done();
+    });
+  });
+});
